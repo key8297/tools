105733942514a5e8578c753040ff839f60a80ae2
Chong Han Khee <chong.han.khee@exact.com>
2022-07-06 13:00:49 +0800

    jquery.template.js use html template

---
+++ jquery.template.js
@@ -0,0 +1,20 @@
+const ApplyTemplateValues = (template, data) => {
+    if(!data) return;
+
+    var pattern = /(?<={{)\w+(?=}})/g;
+
+    var html = template;
+    var matches = html.match(pattern);    
+
+    matches.forEach(name => {        
+            if(data[name]) 
+                html = html.replace(`{{${name}}}`, data[name]);   
+        });
+
+    return html;
+}
+
+$.fn.applyTemplateValues = function(data) {        
+        var html = ApplyTemplateValues($(this).html(), data);
+        $(this).html(html); 
+    };
